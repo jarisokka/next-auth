ca4f03726d41971be54c7e56976961e568d51966
Jari Sokka <sokkajar@gmail.com>
2024-11-12 16:44:10 +0200

    WIP login form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+"use client"
+
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      Hello!
-    </div>
+    <main className="pt-10 flex h-full flex-col items-center justify-center">
+      <div className="space-y-6 text-center">
+        <h1 className="font-bold text-white text-6xl">
+          Login
+        </h1>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
