1e7454126f5d75e1d748fcb601c57fa9c595cd62
Jari Sokka <sokkajar@gmail.com>
2024-11-14 15:42:52 +0200

    set auth config

--- auth.ts
+++ auth.ts
@@ -1,5 +1,11 @@
 import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
   providers: [],
 });
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,5 @@
 export { auth as middleware } from "@/auth";
+
+export const config = {
+  mathcher: ["/((?!.+\\.[\\w]+$|_next).*", "/", "/(api|trpc)(.*))"],
+};
