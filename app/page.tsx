d0d8a49ac348b4d6e4e6f6dfb24543a837617ec6
Jari Sokka <sokkajar@gmail.com>
2024-11-14 15:16:16 +0200

    install next auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-"use client"
+"use client";
 
 import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="pt-10 flex h-full flex-col items-center justify-center">
+    <main className="flex h-full flex-col items-center justify-center pt-10">
       <div className="space-y-6 text-center">
-        <h1 className="font-bold text-white text-6xl">
-          Login
-        </h1>
+        <h1 className="text-6xl font-bold text-white">Welcome</h1>
         <div>
           <LoginButton>
             <Button variant="secondary" size="lg">
